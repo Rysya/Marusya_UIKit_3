e13276cbf8c2af33df055e3996f17459e5d66d07
Rysya <pbl6a@bk.ru>
2025-11-13 12:11:06 +0300

    Initial Commit

---
+++ Marusya_HW_UIK_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marusya_HW_UIK_3
+//
+//  Created by Мария Александрова on 13.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
