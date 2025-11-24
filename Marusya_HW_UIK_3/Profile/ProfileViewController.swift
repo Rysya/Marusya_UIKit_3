ebb7ab9b346fc977e272ce9a306716fd74a4a842
Rysya <pbl6a@bk.ru>
2025-11-24 22:24:00 +0300

    complete homework v4.2

--- Marusya_HW_UIK_3/Profile/ProfileViewController.swift
+++ Marusya_HW_UIK_3/Profile/ProfileViewController.swift
@@ -2,17 +2,13 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    private var profileView = ProfileHeaderView()
+    override func loadView() {
+        view = ProfileHeaderView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Профиль пользователя"
         self.view.backgroundColor = .backgroundProfileGray
-        view.addSubview(profileView)
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        profileView.frame = view.bounds
     }
 }
