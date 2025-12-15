955fca41aefec915a417ee9be714542585ce5215
Rysya <pbl6a@bk.ru>
2025-12-15 19:57:44 +0300

    complete hw vk login fix trivial comments

--- Marusya_HW_UIK_3/Profile/LogInViewController.swift
+++ Marusya_HW_UIK_3/Profile/LogInViewController.swift
@@ -2,10 +2,15 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
-    var scrollView: UIScrollView!
-    var isKeyboardVisible = false
+    private var isKeyboardVisible = false
     
-    private lazy var logoVk: UIImageView = {
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.keyboardDismissMode = .interactive
+        return scrollView
+    }()
+    
+    private let logoVk: UIImageView = {
         let imageName = "logo_vk"
         let logoImageView = UIImageView()
         if let image = UIImage(named: imageName) {
@@ -19,7 +24,7 @@ class LogInViewController: UIViewController {
         return logoImageView
     }()
     
-    private lazy var emailTextField: UITextField = {
+    private let emailTextField: UITextField = {
         let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
         let textField = UITextField()
         textField.leftView = leftPaddingView
@@ -50,7 +55,7 @@ class LogInViewController: UIViewController {
         return textField
     }()
     
-    private let loginButton: UIButton = {
+    private lazy var loginButton: UIButton = {
         let button = UIButton()
         button.setTitle("Log In", for: .normal)
         let imageName = "logIn"
@@ -67,14 +72,18 @@ class LogInViewController: UIViewController {
         button.layer.cornerRadius = 10
         button.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
         button.layer.masksToBounds = true
-        button.addTarget(self, action: #selector(toProfilView), for: .touchUpInside)
+        button.addTarget(self,
+                         action: #selector(toProfilView),
+                         for: .touchUpInside)
         return button
     }()
     
     private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+        let stackView = UIStackView(arrangedSubviews: [emailTextField,
+                                                       line,
+                                                       passwordTextField])
         stackView.axis = .vertical
-        stackView.distribution = .fillEqually
+        stackView.distribution = .fillProportionally
         stackView.layer.borderColor = UIColor.lightGray.cgColor
         stackView.layer.borderWidth = 0.5
         stackView.layer.cornerRadius = 10
@@ -84,36 +93,21 @@ class LogInViewController: UIViewController {
     }()
     
     @objc private func toProfilView() {
-        print("идем в профиль")
         let profileVC = ProfileViewController()
-        // Если есть navigationController - используем push
-        if let navigationController = navigationController {
-            navigationController.pushViewController(profileVC, animated: true)
-        } else {
-            // Иначе present
-            profileVC.modalPresentationStyle = .fullScreen
-            present(profileVC, animated: true)
-        }
+        navigationController?.pushViewController(profileVC, animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        setupScrollView()
+        setupSubviews()
         setupConstraints()
         setupHideKeyboardOnTap()
     }
     
-    private func setupScrollView() {
-        scrollView = UIScrollView()
-        scrollView.backgroundColor = .clear
-        setupSubviews()
-        scrollView.keyboardDismissMode = .interactive
-        view.addSubviews([scrollView])
-    }
-    
     private func setupSubviews() {
-        scrollView.addSubviews([logoVk, stackView, line, loginButton])
+        view.addSubviews([scrollView])
+        scrollView.addSubviews([logoVk, stackView, loginButton])
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -136,18 +130,18 @@ class LogInViewController: UIViewController {
               let ks = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
         else { return }
         isKeyboardVisible = true
-        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: ks.height - self.view.safeAreaInsets.bottom + 20, right: 0)
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: ks.height - view.safeAreaInsets.bottom + 20, right: 0)
         let contentHeightMaxY = loginButton.frame.maxY
         scrollView.contentSize = CGSize(width: view.frame.width, height: contentHeightMaxY)
         
-        let maxOffset = max(0, self.scrollView.contentSize.height - self.scrollView.bounds.height + self.scrollView.contentInset.bottom)
-        self.scrollView.contentInset = contentInsets
-        self.scrollView.scrollIndicatorInsets = contentInsets
-        self.scrollView.setContentOffset(CGPoint(x: 0, y: maxOffset), animated: true)
+        let maxOffset = max(0, scrollView.contentSize.height - scrollView.bounds.height + scrollView.contentInset.bottom)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        scrollView.setContentOffset(CGPoint(x: 0, y: maxOffset), animated: true)
     }
     
     @objc func keyboardWillHide(_ notification: NSNotification) {
-        self.scrollView.contentInset = .zero
+        scrollView.contentInset = .zero
         isKeyboardVisible = false
     }
     
@@ -172,33 +166,30 @@ class LogInViewController: UIViewController {
     private func setupConstraints() {
         
         NSLayoutConstraint.activate([
-            scrollView.heightAnchor.constraint(equalTo: self.view.heightAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
-            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
-            scrollView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            scrollView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            scrollView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             
             stackView.topAnchor.constraint(equalTo: logoVk.bottomAnchor, constant: 120),
-            stackView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor, constant: -16),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
             stackView.heightAnchor.constraint(equalToConstant: 100),
             
-            logoVk.topAnchor.constraint(equalTo: self.scrollView.topAnchor, constant: 120),
-            logoVk.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor),
+            logoVk.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 120),
+            logoVk.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             logoVk.widthAnchor.constraint(equalToConstant: 100),
             logoVk.heightAnchor.constraint(equalToConstant: 100),
             
             line.heightAnchor.constraint(equalToConstant: 0.5),
-            line.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 50),
-            line.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor, constant: 16),
-            line.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor, constant: -16),
             
             loginButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 16),
-            loginButton.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor),
+            loginButton.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             loginButton.heightAnchor.constraint(equalToConstant: 50),
-            loginButton.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor, constant: 16),
-            loginButton.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor, constant: -16),
-            loginButton.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor, constant: -20)
+            loginButton.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            loginButton.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            loginButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20)
         ])
     }
 }
